69f0589422aaec9d76d023545f15f568acbae7db
jestoniandales25 <andalesjestoni1998@gmail.com>
2024-09-23 11:52:09 +0800

    Added some button and frames

--- screens/OBSfirstpage.tsx
+++ screens/OBSfirstpage.tsx
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
 import OBSfirstpageStyles from '../styles/OBSfirstpageStyles';
-import { useNavigation } from '@react-navigation/native';
-import { StackActions } from '@react-navigation/native';
 
 export default function OBSfirstpage() {
     return (
       <View style={OBSfirstpageStyles.container}>
-        <Text style={OBSfirstpageStyles.textOutside}>OBPfirstpage</Text>
+        <Text style={OBSfirstpageStyles.textOutside}>Welcome!</Text>
+        <Text style={OBSfirstpageStyles.ViewFrame}>Sample</Text>
+        <TouchableOpacity style={OBSfirstpageStyles.button} onPress={() => alert('Button Pressed')}><Text style={OBSfirstpageStyles.buttonText}>Next</Text></TouchableOpacity>
       </View>
     )
 }
--- styles/OBSfirstpageStyles.tsx
+++ styles/OBSfirstpageStyles.tsx
@@ -13,8 +13,27 @@ const OBSfirstpage = StyleSheet.create({
         fontStyle: 'italic',
         fontSize: 20,
         fontWeight: 'bold',
+        borderColor: 'black',
+        borderWidth: 1,
+        marginBottom:10 
+    },
+    ViewFrame: {
+        
+        borderColor: 'black',
+        borderWidth: 1,
+        marginBottom:10 
+    },
+    button: {
+        paddingVertical: 20,
+        paddingHorizontal: 50,
+        borderRadius: 4,
+        backgroundColor: "black",
+    },
+    buttonText: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
     }
-  
 });
 
 export default OBSfirstpage;
