b1e65845f604d92851e9bdd49eca6059960fe4ef
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2024-11-19 00:22:11 +0800

    removed universal wrapper

--- components/Wrapper.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { View } from 'react-native';
-import styles from "../styles/WrapperStyles";
-
-export default function Wrapper({ children }){
-  return <View style={styles.wrapper}>{children}</View>;
-};
