62349ac7b7388badd20fa101f99ba772f4a74c89
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2024-11-28 13:18:16 +0800

    renamed files for login system

---
+++ screens/SigningAsScreen.tsx
@@ -0,0 +1,24 @@
+import React, { View, Text, TouchableOpacity } from "react-native";
+import styles from "../styles/LoginSystemStyles";
+import Logo from "../assets/images/Logo.svg";
+
+export default function SigningAs({ navigation }) {
+  const handleLogin = () => {
+    navigation.push("loginScreens");
+  };
+  const handleGuest = () => {
+    navigation.push("InteractiveMap");
+  };
+  return (
+    <View style={styles.wrapper}>
+      <Logo width={240} height={86} />
+      <Text style={styles.subtitle}>Navigate the Campus, Your Way.</Text>
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.buttonTextLogin}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.guestButton} onPress={handleGuest}>
+        <Text style={styles.buttonTextGuest}>Continue as Guest</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
