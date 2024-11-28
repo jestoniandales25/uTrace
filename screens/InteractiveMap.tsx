736a230e0749173bb5e8ee53ee1dba4ee4a24d2a
jheartandales25 <161684321+jestoniandales25@users.noreply.github.com>
2024-11-28 16:42:45 +0800

    User Account Interface Front End (Done)

--- screens/InteractiveMap.tsx
+++ screens/InteractiveMap.tsx
@@ -4,8 +4,28 @@ import { WebView } from "react-native-webview";
 import styles from "../styles/InteractiveMapStyles";
 import SearchIcon from "../assets/images/magnifying-glass-solid.svg";
 import UserIcon from "../assets/images/circle-user-solid.svg";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 
 export default function InteractiveMap() {
+  const today = new Date();
+  const formattedDate = today.toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+  const yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1)
+  const formattedYesterday = yesterday.toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  const [isUserPopupVisible, setUserPopupVisible] = useState(false);
+  const toggleUserPopup = () => {
+    setUserPopupVisible(!isUserPopupVisible);
+  };
   //LOG DATA LOCATION TEST, will be removed after debugging
   const handleWebViewMessage = (event: any) => {
     const data = JSON.parse(event.nativeEvent.data);
@@ -17,6 +37,11 @@ export default function InteractiveMap() {
   const handlePress = () => {
     setIsOn(!isOn);
   };
+  const Divider = () => (
+    <View style={styles.dividerContainer}>
+      <View style={styles.line} />
+    </View>
+  );
 
   return (
     <View style={styles.container}>
@@ -39,7 +64,7 @@ export default function InteractiveMap() {
           />
         </View>
         <View style={styles.userContainer}>
-          <TouchableOpacity style={styles.userButton}>
+          <TouchableOpacity style={styles.userButton} onPress={toggleUserPopup}>
             <UserIcon />
           </TouchableOpacity>
         </View>
@@ -48,6 +73,46 @@ export default function InteractiveMap() {
       <TouchableOpacity style={styles.tryButton} onPress={handlePress}>
         <Text style={styles.buttonText}>Try</Text>
       </TouchableOpacity>
+
+      {isUserPopupVisible && (
+      <View style={styles.popupContainer}>
+        <View style={styles.popupContent}>
+          <View style= {styles.popupHeaderContainer}>
+            <Text style={styles.popupHeaderText}>Account Settings</Text>
+            <TouchableOpacity onPress={toggleUserPopup}>
+              <Ionicons name="close" size={34} color="#666666" />
+            </TouchableOpacity>
+          </View>
+          <Divider />
+          <View style={styles.popupInformationContainer}>
+            <View style={styles.popupUserContainer}>
+              <UserIcon />
+              <Text>User Account 109</Text>
+            </View>
+            <Text style={styles.historyText}>History</Text>
+            <Divider />
+            <Text style={styles.todayText}>
+              Today - {formattedDate}
+            </Text>
+            <View style={styles.popupHistoryContainer}>
+                <Text style={styles.popupHistoryText}>Room 09 - 202 | ICT Building | 2nd Floor</Text>
+                <Text style={styles.popupHistoryText}>Room 09 - 306 | ICT Building | 3rd Floor</Text> 
+            </View>
+            <Text style={styles.todayText}>
+              Yesterday - {formattedYesterday}
+            </Text>
+            <View style={styles.popupHistoryContainer}>
+                <Text style={styles.popupHistoryText}>Room 09 - 202 | ICT Building | 2nd Floor</Text> 
+            </View>
+            <Divider /> 
+            <TouchableOpacity style={styles.logoutButton}>
+              <Text style={styles.logoutButtonText}>Log Out</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    )}
+
     </View>
   );
 }
--- styles/InteractiveMapStyles.tsx
+++ styles/InteractiveMapStyles.tsx
@@ -1,3 +1,4 @@
+import { popup } from "leaflet";
 import { Dimensions, StyleSheet } from "react-native";
 
 const { width, height } = Dimensions.get("window");
@@ -86,6 +87,110 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginBottom: 20,
   },
+  popupContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)", 
+  },
+  popupContent: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+    width: "80%",
+    alignItems: "center",
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  popupHeaderContainer: {
+    paddingRight: 20,
+    paddingBottom: 10,
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+  popupHeaderText: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    fontSize: 16,
+    textAlign: "left",
+  },
+  popupInformationContainer: {
+    left: 0,
+    right: 0,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    width: "100%",
+  },
+  popupUserContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+    paddingTop: 10,
+    paddingBottom: 20,
+  },
+  popupHistoryContainer: {
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  popupHistoryText: {
+    paddingTop: 5,
+    paddingBottom: 5,
+    fontSize: 12,
+    textAlign: "left",
+    alignSelf: "stretch",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  historyText: {
+    marginHorizontal: 10,
+    color: "#777",
+    fontSize: 12,
+  },
+  todayText: {
+    marginTop: 10,
+    marginHorizontal: 10,
+    color: "#777",
+    fontSize: 14,
+  },
+
+  logoutButton: {
+    marginTop: 30,
+    padding: 10,
+    backgroundColor: "#FDB218",
+    borderRadius: 5,
+    alignItems: "center",
+    width: "100%",
+  },
+  logoutButtonText: {
+    color: "#fff",
+    fontSize: 14,
+  },
+  
+  //Divider
+  dividerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+  },
+  line: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "#ccc",
+  },
+
 });
 
 export default styles;
